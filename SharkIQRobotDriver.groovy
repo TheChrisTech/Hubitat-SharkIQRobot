7e74193946e9a3b0a166f2425b12ca7b40de4319
Chris <32913587+TheChrisTech@users.noreply.github.com>
2020-10-17 00:52:21 -0400

    Update SharkIQRobotDriver.groovy
    Revision for newer AlyaNetworks API endpoints
    Support for Multi Devices (Just create Multiple Drivers)
    Spoof iOS or Android Devices when making API calls.
    Cleanup Code for future enhancements (eg. Power Settings)

--- SharkIQRobotDriver.groovy
+++ SharkIQRobotDriver.groovy
@@ -17,7 +17,8 @@
  *    Date        Who            What
  *    ----        ---            ----
  *    2020-02-13  Chris Stevens  Original Creation
- *    2020-10-16  Chris Stevens  Initial 'Public'
+ *    2020-10-16  Chris Stevens  Initial 'Public' Release
+ *    2020-10-17  Chris Stevens  Revision for newer AlyaNetworks API endpoints - Support for Multi Devices (Just create Multiple Drivers) - Spoof iOS or Android Devices when making API calls.
  *
  */
 
@@ -33,6 +34,8 @@ metadata {
     preferences {
         input(name: "loginusername", type: "string", title:"Email", description: "Shark Account Email Address", required: true, displayDuringSetup: true)
         input(name: "loginpassword", type: "password", title:"Password", description: "Shark Account Password", required: true, displayDuringSetup: true)
+        input(name: "sharkdevicename", type: "string", title:"Device Name", description: "Name you've given your Shark Device within the App", required: true, displayDuringSetup: true)
+        input(name: "mobiletype", type: "enum", title:"Mobile Device", description: "Type of Mobile Device your Shark is setup on", required: true, displayDuringSetup: true, options:["Apple iOS", "Android OS"])
     }
 }
  
@@ -40,17 +43,18 @@ def parse(String description) {
     log.debug(description)
 }
  
-def push(String action) {
+def push(String action, String operation, Integer operationValue) {
     def authtoken = ''
     def uuid = ''
+    def dsnForDevice = ''
     //toggle the switch to generate events for anything that is subscribed
     sendEvent(name: "switch", value: "on", isStateChange: true)
     runIn(1, toggleOff)
     //sendEvent(name: "switch", value: "off", isStateChange: true)
     login()
-    //getDevices()
+    getDevices()
     getUserProfile()
-    runCmd(action)
+    runCmd(action, operation, operationValue)
 }
  
 def toggleOff() {
@@ -58,16 +62,26 @@ def toggleOff() {
 }
  
 def on() {
-    push("on")
+    push("on", "SET_Operating_Mode", 2)
 }
  
 def off() {
-    push("off")
+    push("off", "SET_Operating_Mode", 3)
 }
 
 def login() {
     def localDevicePort = (devicePort==null) ? "80" : devicePort
-	def body = """{"user":{"email":"$loginusername","application":{"app_id":"Shark-iOS-field-id","app_secret":"Shark-iOS-field-_wW7SiwgrHN8dpU_ugCattOoDk8"},"password":"$loginpassword"}}"""
+    def app_id = ""
+    def app_secret = ""
+    if (mobiletype == "Apple iOS") {
+        app_id = "Shark-iOS-field-id"
+        app_secret = "Shark-iOS-field-_wW7SiwgrHN8dpU_ugCattOoDk8"
+    }
+    else if (mobiletype == "Android OS") {
+        app_id = "Shark-Android-field-id"
+        app_secret = "Shark-Android-field-Wv43MbdXRM297HUHotqe6lU1n-w"
+    }
+	def body = """{"user":{"email":"$loginusername","application":{"app_id":"$app_id","app_secret":"$app_secret"},"password":"$loginpassword"}}"""
     
     //log.info body
 	def params = [
@@ -128,9 +142,19 @@ def getDevices() {
     ]
     try {
     httpGet(params) { response ->
-        
         if(response.getStatus() == 200 || response.getStatus() == 201) {
 			log.debug "Response received from Shark in the postReponseHandler. $response.data"
+            def devicedsn = ""
+            for (devices in response.data.device ) {
+                if ("$sharkdevicename" == "${devices.product_name}")
+                {   
+                    dsnForDevice = "${devices.dsn}"
+                }
+            }
+            if ("$dsnForDevice" == '')
+            {
+                log.error "$sharkdevicename did not match any product_name on your account. Please verify your `Device Name`."
+            }
     	}
         else {
     		log.error "Shark failed. Shark returned ${response.getStatus()}."
@@ -143,23 +167,16 @@ def getDevices() {
 
 }
  
-def runCmd(String action) {
+def runCmd(String action, String operation, Integer operationValue) {
     def localDevicePort = (devicePort==null) ? "80" : devicePort
-	def body = ''
-   
-	if (action == "on") {
-			body = '{"datapoint":{"value":2,"metadata":{"userUUID":"$uuid"}}}'
-	}
-	else if (action == "off") {
-			body = '{"datapoint":{"value":3,"metadata":{"userUUID":"$uuid"}}}'
-	}
 	def params = [
         uri: "https://ads-field.aylanetworks.com",
-		path: "/apiv1/properties/171735600/datapoints.json",
+		path: "/apiv1/dsns/$dsnForDevice/properties/$operation/datapoints.json",
         requestContentType: "application/json",
         headers: ["Content-Type": "application/json", "Accept": "*/*", "Authorization": "auth_token $authtoken"],
-        body: "$body"
+        body: "{\"datapoint\":{\"value\":\"$operationValue\",\"metadata\":{\"userUUID\":\"$uuid\"}}}"
     ]
+    log.debug "$params"
     try {
     httpPost(params) { response ->
         
